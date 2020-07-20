8f85c22788d919fdeed848e9d4ac6a7d690af3aa
Andrew-Masnyi <andreyolyalya@gmail.com>
2020-07-20 15:27:16 +0300

    slick slider start

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,19 @@ if (process.env.NODE_ENV === 'development') {
 }
 console.log('webpack starterkit');
 
+$(document).ready(function ($) {
+    $('.slider').slick({
+        asNavFor: '.text-slider',
+        arrows: true,
+        centerMode: true,
+        slidesToShow: 1,
+        nextArrow: '<div class="slider">',
+        prevArrow: '<div class="slider">'
+    });
+});
+
+$(document).ready(function(){
+    $('.text-slider').slick({
+        arrows: false,
+    });
+  });
