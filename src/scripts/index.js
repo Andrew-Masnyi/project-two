5bc6e68b6c6fdc31ed777e3e8afd8a90ae4dc468
Andrew-Masnyi <andreyolyalya@gmail.com>
2020-07-21 16:36:53 +0300

    section 7-8

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,16 +7,18 @@ console.log('webpack starterkit');
 $(document).ready(function ($) {
     $('.slider').slick({
         asNavFor: '.text-slider',
+        autoplay: true,
         arrows: true,
         centerMode: true,
         slidesToShow: 1,
-        nextArrow: '<div class="slider">',
-        prevArrow: '<div class="slider">'
+        // nextArrow: '<div class="slider">',
+        // prevArrow: '<div class="slider">'
     });
 });
 
 $(document).ready(function(){
     $('.text-slider').slick({
+        asNavFor: '.slider',
         arrows: false,
     });
   });
