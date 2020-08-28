009b7ca9734230f0d8818868ae9a4b20d9e374ee
Andrew-Masnyi <andreyolyalya@gmail.com>
2020-08-28 13:14:01 +0300

    no message

---
+++ src/scripts/index.js
@@ -0,0 +1,39 @@
+import '../styles/index.scss';
+if (process.env.NODE_ENV === 'development') {
+    require('../index.html');
+}
+console.log('webpack starterkit');
+
+$(document).ready(function ($) {
+    $('.slider').slick({
+        infinite: false,
+        asNavFor: '.text-slider',
+        autoplay: false,
+        arrows: true,
+        centerMode: true,
+        centerPadding: '200px',
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        initialSlide: 1,
+        responsive: [
+            {
+                breakpoint:  1199,
+                settings:   {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: false,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function(){
+    $('.text-slider').slick({
+        asNavFor: '.slider',
+        arrows: false,
+        infinite: false,
+        initialSlide: 1,
+    });
+  });
